2a6cae3282e6bda99e4ffcbb6d8cef158b021d2b
Pacinho19 <pacio170@interia.pl>
2023-02-01 10:39:43 +0100

    Initial commit

---
+++ src/main/java/pl/pracinho/panguingue/PanguingueApplication.java
@@ -0,0 +1,13 @@
+package pl.pracinho.panguingue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanguingueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanguingueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pracinho/panguingue/PanguingueApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pracinho.panguingue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanguingueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
